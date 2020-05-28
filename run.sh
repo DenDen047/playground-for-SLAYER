37855acbebf367d97f9073e98d30c3c0b1951f08
DenDen047 <sh.mn.nat@gmail.com>
2020-05-28 19:23:00 +0900

    success to build the container

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 CURRENT_PATH=$(pwd)
-IMAGE_NAME="denden047/bindsnet_env:latest"
+IMAGE_NAME="denden047/slayer_env:latest"
 
 docker build -t ${IMAGE_NAME} ./docker && \
+docker rmi $(docker images -f "dangling=true" -q) && \
 docker run -it --rm \
     --gpus '"device=0"' \
     -v ${CURRENT_PATH}/src/:/src/ \
     -v ${CURRENT_PATH}/data/:/data/ \
     -w /src \
-    ${IMAGE_NAME}
+    ${IMAGE_NAME} \
+    /bin/bash
